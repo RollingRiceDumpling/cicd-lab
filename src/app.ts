3fb2086e2c8ae5af49ee5ce2521a4807cee46926
張九允 <136954539+RollingRiceDumpling@users.noreply.github.com>
2026-05-19 11:14:58 +0000

    fix: restore prettier formatting in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
-import Fastify, { FastifyServerOptions } from 'fastify'
-
+import Fastify, { FastifyServerOptions } from 'fastify';
 
 export function buildApp(options: FastifyServerOptions = {}) {
   const app = Fastify({
